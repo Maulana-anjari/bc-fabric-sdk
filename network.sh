c0fa87396c7046a97b1f903b9a132cf96244ca3a
Guntur D Putra <guntur.dharma@gmail.com>
2022-06-09 09:31:01 +1000

    Restructure the directory and add create channel
    However, this commit has error: no Raft Leader

--- network.sh
+++ network.sh
@@ -5,39 +5,21 @@
 # (c) 2022
 
 # Delete existing artifacts
-rm -rf ./organizations/ordererOrganizations ./organizations/peerOrganizations
 if [ -d channel-artifacts ]; then
   rm -rf ./channel-artifacts
 fi
+if [ -d ./organizations/ ]; then
+  rm -rf ./organizations
+fi
 
-# Generate Crypto artifacts for organizations
-echo "==================================== Generating Cryptos ========================================================================"
-cryptogen generate --config=./config/crypto-config.yaml --output=./organizations/
+# Stop the network (if any)
+docker-compose -f ./config/docker/docker-compose.yaml down --volumes --remove-orphans
 
-# System channel
-SYS_CHANNEL="sys-channel"
+# Create artifacts
+scripts/createArtifacts.sh
 
-# channel name defaults to "mychannel"
-CHANNEL_NAME="slinmac"
+# Start the network
+docker-compose -f ./config/docker/docker-compose.yaml up -d
 
-echo $CHANNEL_NAME
-
-# Generate System Genesis block
-echo "==================================== Generating Orderer Genesis ========================================================================"
-configtxgen -profile OrdererGenesis -configPath ./config/ -channelID $SYS_CHANNEL  -outputBlock ./channel-artifacts/genesis.block
-
-echo "==================================== Generating Channel Genesis ========================================================================"
-# Generate channel configuration block
-configtxgen -profile BasicChannel -configPath ./config/ -outputCreateChannelTx ./channel-artifacts/slinmac.tx -channelID $CHANNEL_NAME
-
-echo "==================================== Generating Anchor OperatorA ========================================================================"
-configtxgen -profile BasicChannel -configPath ./config/ -outputAnchorPeersUpdate ./channel-artifacts/OperatorAMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OperatorAMSP
-
-echo "==================================== Generating Anchor OperatorB ========================================================================"
-configtxgen -profile BasicChannel -configPath ./config/ -outputAnchorPeersUpdate ./channel-artifacts/OperatorBMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OperatorBMSP
-
-echo "==================================== Generating Anchor OperatorC ========================================================================"
-configtxgen -profile BasicChannel -configPath ./config/ -outputAnchorPeersUpdate ./channel-artifacts/OperatorCMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OperatorCMSP
-
-# echo "==================================== Generating Connection Profiles ===================================="
-# source ccp-generate.sh
+# Create channel
+scripts/createChannel.sh
---
+++ scripts/createChannel.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Note: this file is to be called from ../network.sh
+
+# Configure environment variables
+export CORE_PEER_TLS_ENABLED=true
+export IAEA_CA=${PWD}/organizations/ordererOrganizations/iaea.org/orderers/orderer.iaea.org/msp/tlscacerts/tlsca.iaea.org-cert.pem
+
+export PEER0_OPERATORA_CA=${PWD}/organizations/peerOrganizations/operator-a.com.au/peers/peer0.operator-a.com.au/tls/ca.crt
+export PEER0_OPERATORA_PORT=7051
+
+export PEER0_OPERATORB_CA=${PWD}/organizations/peerOrganizations/operator-b.com.au/peers/peer0.operator-b.com.au/tls/ca.crt
+export PEER0_OPERATORB_PORT=8051
+
+export PEER0_OPERATORC_CA=${PWD}/organizations/peerOrganizations/operator-c.co.nz/peers/peer0.operator-c.co.nz/tls/ca.crt
+export PEER0_OPERATORC_PORT=9051
+
+export FABRIC_CFG_PATH=${PWD}/config/
+
+export IAEA_PORT=5050
+export IAEA_HOST=orderer.iaea.org
+
+CHANNEL_NAME="slinmac"
+
+########################################################################################################################
+# Functions definition
+
+setGlobalsForPeer0OperatorA(){
+    export CORE_PEER_LOCALMSPID="OperatorAMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORA_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-a.com.au/users/Admin@operator-a.com.au/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORA_PORT
+}
+
+createChannel(){
+    # rm -rf ./channel-artifacts/*
+    setGlobalsForPeer0OperatorA
+    
+    peer channel create -o localhost:$IAEA_PORT -c $CHANNEL_NAME \
+    --ordererTLSHostnameOverride $IAEA_HOST \
+    -f ./channel-artifacts/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
+    --tls $CORE_PEER_TLS_ENABLED --cafile $IAEA_CA
+}
+
+# Start here
+createChannel
+# joinChannel
+# updateAnchorPeers
