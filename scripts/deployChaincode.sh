0d770aa5bf3c10ca09b94869b70f3b407cf24747
novaldypratama <novaldypratama@mail.ugm.ac.id>
2023-10-17 17:44:26 +0700

    refactor: adjust the variable names and values in chaincode scripts

--- scripts/deployChaincode.sh
+++ scripts/deployChaincode.sh
@@ -4,56 +4,56 @@
 
 # Configure environment variables
 export CORE_PEER_TLS_ENABLED=true
-export IAEA_CA=${PWD}/organizations/ordererOrganizations/iaea.org/orderers/orderer.iaea.org/msp/tlscacerts/tlsca.iaea.org-cert.pem
+export ARSADA_CA=${PWD}/organizations/ordererOrganizations/arsada.org/orderers/orderer.arsada.org/msp/tlscacerts/tlsca.arsada.org-cert.pem
 
-export PEER0_OPERATORA_CA=${PWD}/organizations/peerOrganizations/operator-a.com.au/peers/peer0.operator-a.com.au/tls/ca.crt
-export PEER0_OPERATORA_PORT=7051
+export PEER0_SARDJITO_CA=${PWD}/organizations/peerOrganizations/sardjito.co.id/peers/peer0.sardjito.co.id/tls/ca.crt
+export PEER0_SARDJITO_PORT=7051
 
-export PEER0_OPERATORB_CA=${PWD}/organizations/peerOrganizations/operator-b.com.au/peers/peer0.operator-b.com.au/tls/ca.crt
-export PEER0_OPERATORB_PORT=8051
+export PEER0_RSCM_CA=${PWD}/organizations/peerOrganizations/rscm.co.id/peers/peer0.rscm.co.id/tls/ca.crt
+export PEER0_RSCM_PORT=8051
 
-export PEER0_OPERATORC_CA=${PWD}/organizations/peerOrganizations/operator-c.co.nz/peers/peer0.operator-c.co.nz/tls/ca.crt
-export PEER0_OPERATORC_PORT=9051
+export PEER0_DHARMAIS_CA=${PWD}/organizations/peerOrganizations/dharmais.co.id/peers/peer0.dharmais.co.id/tls/ca.crt
+export PEER0_DHARMAIS_PORT=9051
 
 export FABRIC_CFG_PATH=${PWD}/config/
 
-export IAEA_PORT=5050
-export IAEA_HOST=orderer.iaea.org
+export ARSADA_PORT=5050
+export ARSADA_HOST=orderer.arsada.org
 
-CHANNEL_NAME="slinmac"
+CHANNEL_NAME="pharma-chain"
 CC_RUNTIME_LANGUAGE="node"
 VERSION="1"
 CC_SRC_PATH="./chaincode/"
-CC_NAME="contract"
+CC_NAME="pharma-contract"
 
 ########################################################################################################################
 # Functions definition
 
-setGlobalsForPeer0OperatorA(){
-    export CORE_PEER_LOCALMSPID="OperatorAMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORA_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-a.com.au/users/Admin@operator-a.com.au/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORA_PORT
+setGlobalsForPeer0Sardjito(){
+    export CORE_PEER_LOCALMSPID="SardjitoMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_SARDJITO_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/sardjito.co.id/users/Admin@sardjito.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_SARDJITO_PORT
 }
 
-setGlobalsForPeer0OperatorB(){
-    export CORE_PEER_LOCALMSPID="OperatorBMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORB_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-b.com.au/users/Admin@operator-b.com.au/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORB_PORT
+setGlobalsForPeer0RSCM(){
+    export CORE_PEER_LOCALMSPID="RSCMMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_RSCM_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/rscm.co.id/users/Admin@rscm.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_RSCM_PORT
 }
 
-setGlobalsForPeer0OperatorC(){
-    export CORE_PEER_LOCALMSPID="OperatorCMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORC_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-c.co.nz/users/Admin@operator-c.co.nz/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORC_PORT
+setGlobalsForPeer0Dharmais(){
+    export CORE_PEER_LOCALMSPID="DharmaisMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_DHARMAIS_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/dharmais.co.id/users/Admin@dharmais.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_DHARMAIS_PORT
 }
 
 packageChaincode() {
     rm -rf ${CC_NAME}.tar.gz
 
-    setGlobalsForPeer0OperatorA
+    setGlobalsForPeer0Sardjito
     peer lifecycle chaincode package ${CC_NAME}.tar.gz \
         --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} \
         --label ${CC_NAME}_${VERSION}
@@ -61,57 +61,57 @@ packageChaincode() {
 }
 
 installChaincode() {
-    setGlobalsForPeer0OperatorA
+    setGlobalsForPeer0Sardjito
     peer lifecycle chaincode install ${CC_NAME}.tar.gz
-    echo "===================== Chaincode is installed on peer0.OperatorA ===================== "
+    echo "===================== Chaincode is installed on peer0.sardjito ===================== "
 
-    setGlobalsForPeer0OperatorB
+    setGlobalsForPeer0RSCM
     peer lifecycle chaincode install ${CC_NAME}.tar.gz
-    echo "===================== Chaincode is installed on peer0.OperatorB ===================== "
+    echo "===================== Chaincode is installed on peer0.rscm ===================== "
 
-    setGlobalsForPeer0OperatorC
+    setGlobalsForPeer0Dharmais
     peer lifecycle chaincode install ${CC_NAME}.tar.gz
-    echo "===================== Chaincode is installed on peer0.OperatorC ===================== "
+    echo "===================== Chaincode is installed on peer0.dharmais ===================== "
 }
 
 queryInstalled() {
-    setGlobalsForPeer0OperatorC
+    setGlobalsForPeer0Dharmais
     peer lifecycle chaincode queryinstalled >&log.txt
     cat log.txt
     PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
     echo PackageID is ${PACKAGE_ID}
-    echo "===================== Query installed successful on peer0.OperatorC on channel ${CHANNEL_NAME}===================== "
+    echo "===================== Query installed successful on peer0.dharmais on channel ${CHANNEL_NAME}===================== "
 }
 
 approveChaincode() {
 
-    setGlobalsForPeer0OperatorA
-    peer lifecycle chaincode approveformyorg -o localhost:$IAEA_PORT \
-        --ordererTLSHostnameOverride $IAEA_HOST --tls \
-        --cafile $IAEA_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+    setGlobalsForPeer0Sardjito
+    peer lifecycle chaincode approveformyorg -o localhost:$ARSADA_PORT \
+        --ordererTLSHostnameOverride $ARSADA_HOST --tls \
+        --cafile $ARSADA_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --package-id ${PACKAGE_ID} \
         --sequence ${VERSION}
-    echo "===================== chaincode approved from OperatorA ===================== "
+    echo "===================== chaincode approved from SARDJITO ===================== "
 
-    setGlobalsForPeer0OperatorB
-    peer lifecycle chaincode approveformyorg -o localhost:$IAEA_PORT \
-        --ordererTLSHostnameOverride $IAEA_HOST --tls \
-        --cafile $IAEA_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+    setGlobalsForPeer0RSCM
+    peer lifecycle chaincode approveformyorg -o localhost:$ARSADA_PORT \
+        --ordererTLSHostnameOverride $ARSADA_HOST --tls \
+        --cafile $ARSADA_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --package-id ${PACKAGE_ID} \
         --sequence ${VERSION}
-    echo "===================== chaincode approved from OperatorB ===================== "
+    echo "===================== chaincode approved from RSCM ===================== "
 
-    setGlobalsForPeer0OperatorC
-    peer lifecycle chaincode approveformyorg -o localhost:$IAEA_PORT \
-        --ordererTLSHostnameOverride $IAEA_HOST --tls \
-        --cafile $IAEA_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+    setGlobalsForPeer0Dharmais
+    peer lifecycle chaincode approveformyorg -o localhost:$ARSADA_PORT \
+        --ordererTLSHostnameOverride $ARSADA_HOST --tls \
+        --cafile $ARSADA_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --package-id ${PACKAGE_ID} \
         --sequence ${VERSION}
-    echo "===================== chaincode approved from OperatorC ===================== "
+    echo "===================== chaincode approved from DHARMAIS ===================== "
 }
 
 checkCommitReadyness() {
-    setGlobalsForPeer0OperatorA
+    setGlobalsForPeer0Sardjito
     peer lifecycle chaincode checkcommitreadiness \
         --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --sequence ${VERSION} --output json
@@ -119,19 +119,19 @@ checkCommitReadyness() {
 }
 
 commitChaincodeDefinition() {
-    setGlobalsForPeer0OperatorA
-    peer lifecycle chaincode commit -o localhost:$IAEA_PORT --ordererTLSHostnameOverride $IAEA_HOST \
-        --tls $CORE_PEER_TLS_ENABLED --cafile $IAEA_CA \
+    setGlobalsForPeer0Sardjito
+    peer lifecycle chaincode commit -o localhost:$ARSADA_PORT --ordererTLSHostnameOverride $ARSADA_HOST \
+        --tls $CORE_PEER_TLS_ENABLED --cafile $ARSADA_CA \
         --channelID $CHANNEL_NAME --name ${CC_NAME} \
-        --peerAddresses localhost:$PEER0_OPERATORA_PORT --tlsRootCertFiles $PEER0_OPERATORA_CA \
-        --peerAddresses localhost:$PEER0_OPERATORB_PORT --tlsRootCertFiles $PEER0_OPERATORB_CA \
-        --peerAddresses localhost:$PEER0_OPERATORC_PORT --tlsRootCertFiles $PEER0_OPERATORC_CA \
+        --peerAddresses localhost:$PEER0_SARDJITO_PORT --tlsRootCertFiles $PEER0_SARDJITO_CA \
+        --peerAddresses localhost:$PEER0_RSCM_PORT --tlsRootCertFiles $PEER0_RSCM_CA \
+        --peerAddresses localhost:$PEER0_DHARMAIS_PORT --tlsRootCertFiles $PEER0_DHARMAIS_CA \
         --version ${VERSION} --sequence ${VERSION}
     echo "===================== Committing Chaincode ===================== "
 }
 
 queryCommitted() {
-    setGlobalsForPeer0OperatorC
+    setGlobalsForPeer0Dharmais
     peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME #--name ${CC_NAME}
 }
 
--- scripts/testChaincode.sh
+++ scripts/testChaincode.sh
@@ -4,38 +4,38 @@
 
 # Configure environment variables
 export CORE_PEER_TLS_ENABLED=true
-export IAEA_CA=${PWD}/organizations/ordererOrganizations/iaea.org/orderers/orderer.iaea.org/msp/tlscacerts/tlsca.iaea.org-cert.pem
+export ARSADA_CA=${PWD}/organizations/ordererOrganizations/arsada.org/orderers/orderer.arsada.org/msp/tlscacerts/tlsca.arsada.org-cert.pem
 
-export PEER0_OPERATORA_CA=${PWD}/organizations/peerOrganizations/operator-a.com.au/peers/peer0.operator-a.com.au/tls/ca.crt
-export PEER0_OPERATORA_PORT=7051
+export PEER0_SARDJITO_CA=${PWD}/organizations/peerOrganizations/sardjito.co.id/peers/peer0.sardjito.co.id/tls/ca.crt
+export PEER0_SARDJITO_PORT=7051
 
-export PEER0_OPERATORB_CA=${PWD}/organizations/peerOrganizations/operator-b.com.au/peers/peer0.operator-b.com.au/tls/ca.crt
-export PEER0_OPERATORB_PORT=8051
+export PEER0_RSCM_CA=${PWD}/organizations/peerOrganizations/rscm.co.id/peers/peer0.rscm.co.id/tls/ca.crt
+export PEER0_RSCM_PORT=8051
 
 export FABRIC_CFG_PATH=${PWD}/config/
-export IAEA_PORT=5050
-export IAEA_HOST=orderer.iaea.org
+export ARSADA_PORT=5050
+export ARSADA_HOST=orderer.arsada.org
 
-CHANNEL_NAME="slinmac"
-CC_NAME="contract"
+CHANNEL_NAME="pharma-chain"
+CC_NAME="pharma-contract"
 
-setGlobalsForPeer0OperatorA(){
-    export CORE_PEER_LOCALMSPID="OperatorAMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORA_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-a.com.au/users/Admin@operator-a.com.au/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORA_PORT
+setGlobalsForPeer0Sardjito(){
+    export CORE_PEER_LOCALMSPID="SardjitoMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_SARDJITO_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/sardjito.co.id/users/Admin@sardjito.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_SARDJITO_PORT
 }
 
-setGlobalsForPeer0OperatorA
+setGlobalsForPeer0Sardjito
 
 echo "Invoking the chaincode: function: 'UploadIdentity'..."
-peer chaincode invoke -o localhost:$IAEA_PORT \
---ordererTLSHostnameOverride $IAEA_HOST \
---tls --cafile $IAEA_CA \
+peer chaincode invoke -o localhost:$ARSADA_PORT \
+--ordererTLSHostnameOverride $ARSADA_HOST \
+--tls --cafile $ARSADA_CA \
 -C $CHANNEL_NAME -n $CC_NAME \
---peerAddresses localhost:$PEER0_OPERATORA_PORT \
---tlsRootCertFiles $PEER0_OPERATORA_CA \
---peerAddresses localhost:$PEER0_OPERATORB_PORT \
---tlsRootCertFiles $PEER0_OPERATORB_CA \
+--peerAddresses localhost:$PEER0_SARDJITO_PORT \
+--tlsRootCertFiles $PEER0_SARDJITO_CA \
+--peerAddresses localhost:$PEER0_RSCM_PORT \
+--tlsRootCertFiles $PEER0_RSCM_CA \
 -c '{"function":"UploadIdentity","Args":["abcdef123"]}'
 echo "============================================================================="
