8f42380d76291a3f97779fb14d9766b6ae983fd3
novaldypratama <novaldypratama@mail.ugm.ac.id>
2023-10-17 17:42:11 +0700

    refactor: adjust the variables name, values, and docker-compose config

--- scripts/createChannel.sh
+++ scripts/createChannel.sh
@@ -4,81 +4,81 @@
 
 # Configure environment variables
 export CORE_PEER_TLS_ENABLED=true
-export IAEA_CA=${PWD}/organizations/ordererOrganizations/iaea.org/orderers/orderer.iaea.org/msp/tlscacerts/tlsca.iaea.org-cert.pem
+export ARSADA_CA=${PWD}/organizations/ordererOrganizations/arsada.org/orderers/orderer.arsada.org/msp/tlscacerts/tlsca.arsada.org-cert.pem
 
-export PEER0_OPERATORA_CA=${PWD}/organizations/peerOrganizations/operator-a.com.au/peers/peer0.operator-a.com.au/tls/ca.crt
-export PEER0_OPERATORA_PORT=7051
+export PEER0_SARDJITO_CA=${PWD}/organizations/peerOrganizations/sardjito.co.id/peers/peer0.sardjito.co.id/tls/ca.crt
+export PEER0_SARDJITO_PORT=7051
 
-export PEER0_OPERATORB_CA=${PWD}/organizations/peerOrganizations/operator-b.com.au/peers/peer0.operator-b.com.au/tls/ca.crt
-export PEER0_OPERATORB_PORT=8051
+export PEER0_RSCM_CA=${PWD}/organizations/peerOrganizations/rscm.co.id/peers/peer0.rscm.co.id/tls/ca.crt
+export PEER0_RSCM_PORT=8051
 
-export PEER0_OPERATORC_CA=${PWD}/organizations/peerOrganizations/operator-c.co.nz/peers/peer0.operator-c.co.nz/tls/ca.crt
-export PEER0_OPERATORC_PORT=9051
+export PEER0_DHARMAIS_CA=${PWD}/organizations/peerOrganizations/dharmais.co.id/peers/peer0.dharmais.co.id/tls/ca.crt
+export PEER0_DHARMAIS_PORT=9051
 
 export FABRIC_CFG_PATH=${PWD}/config/
 
-export IAEA_PORT=5050
-export IAEA_HOST=orderer.iaea.org
+export ARSADA_PORT=5050
+export ARSADA_HOST=orderer.arsada.org
 
-CHANNEL_NAME="slinmac"
+CHANNEL_NAME="pharma-chain"
 
 ########################################################################################################################
 # Functions definition
 
-setGlobalsForPeer0OperatorA(){
-    export CORE_PEER_LOCALMSPID="OperatorAMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORA_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-a.com.au/users/Admin@operator-a.com.au/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORA_PORT
+setGlobalsForPeer0Sardjito(){
+    export CORE_PEER_LOCALMSPID="SardjitoMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_SARDJITO_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/sardjito.co.id/users/Admin@sardjito.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_SARDJITO_PORT
 }
 
-setGlobalsForPeer0OperatorB(){
-    export CORE_PEER_LOCALMSPID="OperatorBMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORB_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-b.com.au/users/Admin@operator-b.com.au/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORB_PORT
+setGlobalsForPeer0RSCM(){
+    export CORE_PEER_LOCALMSPID="RSCMMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_RSCM_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/rscm.co.id/users/Admin@rscm.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_RSCM_PORT
 }
 
-setGlobalsForPeer0OperatorC(){
-    export CORE_PEER_LOCALMSPID="OperatorCMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_OPERATORC_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/operator-c.co.nz/users/Admin@operator-c.co.nz/msp
-    export CORE_PEER_ADDRESS=localhost:$PEER0_OPERATORC_PORT
+setGlobalsForPeer0Dharmais(){
+    export CORE_PEER_LOCALMSPID="DharmaisMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_DHARMAIS_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/dharmais.co.id/users/Admin@dharmais.co.id/msp
+    export CORE_PEER_ADDRESS=localhost:$PEER0_DHARMAIS_PORT
 }
 
 createChannel(){
     # rm -rf ./channel-artifacts/*
-    setGlobalsForPeer0OperatorA
+    setGlobalsForPeer0Sardjito
     
-    peer channel create -o localhost:$IAEA_PORT -c $CHANNEL_NAME \
-    --ordererTLSHostnameOverride $IAEA_HOST \
+    peer channel create -o localhost:$ARSADA_PORT -c $CHANNEL_NAME \
+    --ordererTLSHostnameOverride $ARSADA_HOST \
     -f ./channel-artifacts/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
-    --tls $CORE_PEER_TLS_ENABLED --cafile $IAEA_CA
+    --tls $CORE_PEER_TLS_ENABLED --cafile $ARSADA_CA
 }
 
 joinChannel(){
-    setGlobalsForPeer0OperatorA
+    setGlobalsForPeer0Sardjito
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
 
-    setGlobalsForPeer0OperatorB
+    setGlobalsForPeer0RSCM
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block 
 
-    setGlobalsForPeer0OperatorC
+    setGlobalsForPeer0Dharmais
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
 }
 
 updateAnchorPeers(){
-    setGlobalsForPeer0OperatorA
-    peer channel update -o localhost:$IAEA_PORT --ordererTLSHostnameOverride $IAEA_HOST -c $CHANNEL_NAME \
-    -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $IAEA_CA
+    setGlobalsForPeer0Sardjito
+    peer channel update -o localhost:$ARSADA_PORT --ordererTLSHostnameOverride $ARSADA_HOST -c $CHANNEL_NAME \
+    -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ARSADA_CA
     
-    setGlobalsForPeer0OperatorB
-    peer channel update -o localhost:$IAEA_PORT --ordererTLSHostnameOverride $IAEA_HOST -c $CHANNEL_NAME \
-    -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $IAEA_CA
+    setGlobalsForPeer0RSCM
+    peer channel update -o localhost:$ARSADA_PORT --ordererTLSHostnameOverride $ARSADA_HOST -c $CHANNEL_NAME \
+    -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ARSADA_CA
 
-    setGlobalsForPeer0OperatorC
-    peer channel update -o localhost:$IAEA_PORT --ordererTLSHostnameOverride $IAEA_HOST -c $CHANNEL_NAME \
-    -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $IAEA_CA
+    setGlobalsForPeer0Dharmais
+    peer channel update -o localhost:$ARSADA_PORT --ordererTLSHostnameOverride $ARSADA_HOST -c $CHANNEL_NAME \
+    -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ARSADA_CA
 }
 
 # Start here
